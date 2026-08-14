8e6e2a84b09df6d440197910e70a6243f0bbe015
TrishaKabadge <trishakabadge@gmail.com>
2026-08-14 17:50:50 +0000

    abc

--- fraction.cpp
+++ fraction.cpp
@@ -1,44 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 class fraction
 {
-    private:
-    int a,b,c,d;
+private:
+    int a, b;
 
-    public:
-
+public:
     void input()
     {
-        cin>>a>>b>>c>>d;
+        cin >> a >> b;
     }
+
     void add(fraction f)
     {
-        int num=a*f.b+f.a*b;
-        int den=b*f.b;
-        cout<<"Addition:"<<endl;
-        cout<<"Num:"<<num<<endl;
-        cout<<"Den:"<<den<<endl;
+        int num = a * f.b + f.a * b;
+        int den = b * f.b;
+
+        cout << "Addition:" << endl;
+        cout << "Numerator = " << num << endl;
+        cout << "Denominator = " << den << endl;
     }
+
     void sub(fraction f)
     {
-        int num=(a*f.b)-f.a*b;
-        int den=b*f.b;
-        cout<<"Subtraction:"<<endl;
-        cout<<"Num:"<<num<<endl;
-        cout<<"Den:"<<den<<endl;
+        int num = a * f.b - f.a * b;
+        int den = b * f.b;
+
+        cout << "Subtraction:" << endl;
+        cout << "Numerator = " << num << endl;
+        cout << "Denominator = " << den << endl;
     }
 };
 
 int main()
 {
-    fraction f1,f2;
-    cout<<"Enter a and b:";
+    fraction f1, f2;
+
+    cout << "Enter numerator and denominator of first fraction: ";
     f1.input();
-    cout<<"Enter c and d:";
+
+    cout << "Enter numerator and denominator of second fraction: ";
     f2.input();
 
     f1.add(f2);
     f1.sub(f2);
-    
+
     return 0;
 }
